43a08a5067ec5436eeec641610c80100ccf2e4eb
龚君意 <gongjunyi@gongjunyideMacBook-Air.local>
2020-09-16 17:37:45 +0800

    feat: divider fixed

--- docs/core/layout/divider.md
+++ docs/core/layout/divider.md
@@ -9,26 +9,79 @@ title: Divider 分割线
 ## 简单的分割线
 
 ```tsx
-
+/**
+ * title: 水平分割线
+ * desc: 根据传入的dashed值判断分割线是否为虚线，type默认为horizontal
+ */
 import React from 'react';
-import styled from '@wowjoy/styled';
-
-const P = styled.p`
- margin-bottom: 0
-`
-
-const Space = () => <div style={{ marginTop: 5 }}></div>;
 import { Divider } from '@wowjoy/core';
 
 export default ()=>{
-  const [type, setType] = React.useState("horizontal");
   return(
     <>
-      <span>111111</span>
-      <Divider dashed={true}>Text</Divider>
-      <span>222222</span>
+      <a>Function</a>
+      <Divider dashed={true}/>
+      <a>Object</a>
       <Divider/>
-      <span>333333</span>
+      <a>Array</a>
     </>
   )
 }
+```
+
+## 标题水平分割线
+```tsx
+/**
+ * title: 标题水平分割线
+ * desc: text属性显示标题，orientation控制标题位置
+ */
+import React from 'react';
+import { Divider } from '@wowjoy/core';
+
+export default ()=>{
+  return(
+    <>
+      <a>Function</a>
+      <Divider text={"Text"} orientation="left"/>
+      <a>Object</a>
+      <Divider text={"Text"} orientation="center"/>
+      <a>Array</a>
+      <Divider text={"Text"} orientation="right" dashed={true}/>
+
+    </>
+  )
+}
+```
+
+## 垂直分割线
+
+```tsx
+/**
+ * title: 垂直分割线
+ * desc: 使用 type="vertical" 设置为行内的垂直分割线。
+ */
+import React from 'react';
+import { Divider } from '@wowjoy/core';
+
+export default ()=>{
+  return(
+    <>
+      <a>Function</a>
+      <Divider type={"vertical"} dashed={true}>Text</Divider>
+      <a>Object</a>
+      <Divider type={"vertical"}/>
+      <a>Array</a>
+    </>
+  )
+}
+```
+
+## Props
+
+| 参数 | 说明 | 类型 | 默认值 |
+| :-- | :-- | :-- | :-- |
+| type	 | 水平还是垂直类型	 | horizontal \| vertical | horizontal |
+| dashed	 | 是否虚线		 | boolean | flase |
+| text	 | 标题		 | React.ReactNode |  |
+| orientation	| 分割线标题的位置	 | left \| center \| right	 | center |
+
--- packages/components/src/Divider/index.tsx
+++ packages/components/src/Divider/index.tsx
@@ -1,46 +1,117 @@
 import React from 'react';
 import styled from '@wowjoy/styled';
+import classNames from 'classnames';
 
 
 
 const Line = styled.div<{
-  dashed: boolean;
   type: string;
 }>`
-& {
-  border-width: 1px;
-  border-color: #ccc;
-  border-style: ${ p => ( p.dashed ? 'dashed' : 'solid')};
-  ${ p => (p.type === "vertical" ? `
-    position: relative;
-    top: -.06em;
-    display: inline-block;
-    height: .9em;
-    margin: 0 8px;
-    vertical-align: middle;
-    border-top: 0;
-    border-left: 1px solid rgba(0,0,0,.06);
-  ` : `
+  display: ${ p => p.type === 'vertical' ? 'inline-block' : 'block'};
+  .wowjoy-divider-horizontal{
     display: flex;
     clear: both;
     width: 100%;
     min-width: 100%;
     margin: 24px 0;
-  `)}
-}
+    border-top: 1px solid #999;
+  }
+  .wowjoy-divider-vertical {
+    position: relative;
+    top: -0.06em;
+    display: inline-block;
+    height: 0.9em;
+    margin: 0 8px;
+    vertical-align: middle;
+    border-top: 0;
+    border-left: 1px solid #999;
+  }
+  .wowjoy-divider-horizontal.wowjoy-divider-with-text {
+    display: flex;
+    margin: 16px 0;
+    color: #333;
+    font-weight: 500;
+    font-size: 16px;
+    white-space: nowrap;
+    text-align: center;
+    border-top: 0;
+    &::before,
+    &::after {
+      position: relative;
+      top: 50%;
+      width: 50%;
+      border-top: 1px solid #999;
+      transform: translateY(50%);
+      content: '';
+    }
+  }
+  .wowjoy-divider-horizontal.wowjoy-divider-with-text-left {
+    &::before {
+      top: 50%;
+      width: 5%;
+    }
+    &::after {
+      top: 50%;
+      width: 95%;
+    }
+  }
+  .wowjoy-divider-horizontal.wowjoy-divider-with-text-right {
+    &::before {
+      top: 50%;
+      width: 95%;
+    }
+    &::after {
+      top: 50%;
+      width: 5%;
+    }
+  }
+  .wowjoy-divider-inner-text {
+    display: inline-block;
+    padding: 0 1em;
+  }
+  .wowjoy-divider-dashed {
+    background: none;
+    border-color: #999;
+    border-style: dashed;
+    border-width: 1px 0 0;
+  }
+  .wowjoy-divider-vertical.wowjoy-divider-dashed {
+    border-width: 0 0 0 1px;
+  }
   
 `
 
 export interface Props extends React.HTMLAttributes<HTMLElement> {
   dashed?: boolean;
-  type? :string;
-  indicator?: React.ReactNode;
+  type?: string;
+  text?: React.ReactNode;
+  orientation?: 'left' | 'right' | 'center';
+  className?: string;
 }
 
 const Divider = React.forwardRef<any, Props>(
-  ({ dashed = false, type = "horizontal" }, ref) => {
+  ({ dashed = false, type = 'horizontal',orientation = 'center', className, text, ...restProps }, ref) => {
+    const prefixCls = 'wowjoy-divider';
+    const orientationPrefix = orientation.length > 0 ? `-${orientation}` : orientation;
+    const hasChildren = !!text;
+    const classString = classNames(
+      `${prefixCls}-${type === 'vertical' ? 'vertical' : 'horizontal'}`,
+      {
+        [`${prefixCls}-with-text`]: hasChildren,
+        [`${prefixCls}-with-text${orientationPrefix}`]: hasChildren,
+        [`${prefixCls}-dashed`]: !!dashed,
+      },
+      className
+    )
+
     return (
-        <Line dashed={dashed} type={type}></Line>
+      <Line type = {type}>
+        <div className={classString} {...restProps}>
+        {text && <span className={`${prefixCls}-inner-text`}>{text}</span>}
+      </div>
+
+      </Line>
+      
     );
   },
 );
